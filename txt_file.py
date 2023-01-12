86728e496139f458dbc7b00153e6d6e0cf4cdac4
Hemed Tov <hemedbz@gmail.com>
2023-01-12 18:52:46 +0200

    Implemented len, search, is_in for txt added parts to init

--- txt_file.py
+++ txt_file.py
@@ -1,71 +1,62 @@
 from text_file_parent import TextFile
 from exceptions import *
+import os
 
+class TxtFile(TextFile):
 
-class TxtFile (TextFile):
-    pass
+    def __init__(self, file_path):
+        super().__init__(file_path)
+        self._content = self.get_content()
+        self._words = self._content.split()
+        self.lines = self._content.readlines()
+        self._ext = 'txt'
 
-    def _get_specific_content(self, fd):
+    def _specific_content(self, fd):
         return fd.read()
-
-    def _get_ext(self):
-        return 'txt'
 
     def __add__(self, other):
         if not isinstance(other, TxtFile):
             raise ValueError("2 values must be TxtFile type")
 
-        file_path_name = self.get_root() + "\\" + self.get_file_name() + "_" + other.get_file_name() + \
-                         self.get_file_extension()
+        new_name = self.root + "\\" + self.file_name + "_" + other.file_name + \ self._ext
 
-        if os.path.exists(file_path_name):
-            file_name_exists = f"{self.get_file_name()}_{other.get_file_name()}{self.get_file_extension()}"
-            raise FileNameExistsEror(file_name_exists)
+        if os.path.exists(new_name):
+            raise AlreadyExists(new_name)
 
-        with open(file_path_name, 'w') as fh:
-            fh.write(self.get_content() + other.get_content())
+        with open (new_name, 'x') as fh:
+            fh.write(self._content + other._content)
 
         return True
 
-    def _txt_file_read(self):
-        with open(self._file_path, 'r') as fd:
-            return fd.read()
+    def __len__(self):
+        """
+        :return: tuple with number of characters and number of words
+        """
+        return (len(self._content), len(self._words))
 
-    def __len__(self, *kwargs):
-        pass
-    #     if kwargs[0] == 'c':
-    #         return len(self._txt_file_read())
-    #     elif kwargs[0] == 'w':
-    #         return len(self._txt_file_read().split())
-    #     else:
-    #         raise InvalidInputError(val)
+    def is_in(self, val: str | int) -> bool:
+        if val in self._content:
+            return True
+        else:
+            return False
 
-    def is_in(self, val: str | int) -> bool: #TODO: H
-        pass
+    def search(self, val):
+        """
 
-    def search(self, val): #TODO: H
-        pass
+        :param val: string to search
+        :return: list of tuples with index and line for where str was found
+        """
+        if not self.is_in(val):
+            return None
+        findings = []
+        for index, line in enumerate(self.lines):
+            if val in line:
+                findings.append((index, line))
+        return findings
 
     def add_row(self):
         pass
 
-    def create(self): #TODO: Y
-        if not self.is_exists():
-            f = open(self.get_file_path(), 'x')
-            f.close()
-        else:
-            raise Exception()
-
-    def count(self, val) -> int: #TODO: Y
-        data = self._txt_file_read()
+    def count(self, val) -> int:
+        data = self._content
         return data.count(val)
-
-
-if __name__ == '__main__':
-    alice = TxtFile("features.txt")
-    print(alice.count('header'))
-
-    new_file = TxtFile("D:\\Full_Stack_Python\\Python_Course\\C10\\files\\new_file.txt")
-    new_file.create()
-#
-# #TODO: Ask V about __len__
