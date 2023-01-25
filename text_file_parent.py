703a38d9598e164f588d329741678bdee16c70b7
YaelBenYair <mayael49@telem.openu.ac.il>
2023-01-25 20:35:05 +0200

    add props

--- text_file_parent.py
+++ text_file_parent.py
@@ -10,16 +10,15 @@ class TextFile(ABC):
         self._file_path = file_path
 
         if not os.path.exists(self._file_path):
-            raise FileNotFoundError
+            raise FileNotFoundError()
         if self._ext() != self.get_extension():
             raise TypeError
 
-        # TODO: organize getter setter - Y
         self._content = self.get_content()
-        self.file_size = os.stat(self._file_path).st_size
-        self.root = os.path.dirname(self._file_path)
-        self.base_name = os.path.basename(self._file_path)
-        self.file_name = os.path.splitext(self.base_name)[0]
+        self._file_size = os.stat(self._file_path).st_size
+        self._root = os.path.dirname(self._file_path)
+        self._base_name = os.path.basename(self._file_path)
+        self._file_name = os.path.splitext(self.base_name)[0]
         self._creation_t = self.get_creation_time()
         self._last_modified_t = self._update_last_modified()
 
@@ -34,6 +33,14 @@ class TextFile(ABC):
     def _specific_content(self, fd, *args):
         pass
 
+    @abstractmethod
+    def search(self, val):
+        pass
+
+    @abstractmethod
+    def count(self, val) -> int:
+        pass
+
     def get_content(self, *args):
         with open(self._file_path, 'r') as fd:
             self._content = self._specific_content(fd, *args)
@@ -45,13 +52,12 @@ class TextFile(ABC):
     def get_file_path(self):
         return self._file_path
 
-    @abstractmethod
-    def search(self, val):
-        pass
+    def get_creation_time(self):
+        return os.path.getctime(self._file_path)
 
-    @abstractmethod
-    def count(self, val) -> int:
-        pass
+    def _update_last_modified(self):
+        return os.path.getmtime(self._file_path)
+
 
     @property
     def content(self):
@@ -61,31 +67,43 @@ class TextFile(ABC):
     def content(self, value):
         self._content = value
 
-    def get_creation_time(self):
-        return os.path.getctime(self._file_path)
-
     @property
     def creation_time(self):
         return self._creation_t
 
-    @creation_time.getter
-    def creation_time(self):
-        return time.ctime(self._creation_t)
-
-    def _update_last_modified(self):
-        return os.path.getmtime(self._file_path)
+    # @creation_time.setter
+    # def creation_time(self, value):
+    #     pass
 
     @property
     def last_modified(self):
-        return self._last_modified_t
-
-    @last_modified.getter
-    def last_modified(self):
         return time.ctime(self._last_modified_t)
+    
+    # @last_modified.setter
+    # def last_modified(self, value):
+    #     pass
 
-    @last_modified.setter
-    def last_modified(self):
-        self._update_last_modified()
-        return self._last_modified_t
+    @property
+    def file_path(self):
+        return self._file_path
+
+    @property
+    def file_size(self):
+        return self._file_size
+
+    @property
+    def root(self):
+        return self._root
+
+    @property
+    def base_name(self):
+        return self._base_name
+
+    @property
+    def file_name(self):
+        return self._file_name
+
+
+
 
     #TODO: generator in iter (for each class) - Y
