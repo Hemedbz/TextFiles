1fb257ca214ec3c741453e03d016404d3231b668
Hemed Tov <hemedbz@gmail.com>
2023-04-02 14:33:21 +0300

    search in json

--- textfiles/json_file.py
+++ textfiles/json_file.py
@@ -60,7 +60,40 @@ class JsonFile(TextFile):
         :return: list of findings
         """
 
-ד
+    def search(self, query):
+        if isinstance(self.content, list):
+            return self._search_list(self.content, query)
+        elif isinstance(self.content, dict):
+            return self._search_dict(self.content, query)
+        else:
+            raise TypeError("Unsupported data type")
+
+    @staticmethod
+    def _search_list(data_list, query):
+        findings = []
+        for i, item in enumerate(data_list):
+            if isinstance(item, (list, dict)):
+                sublist_findings = search(item, query)
+                if sublist_findings:
+                    findings.append({f"index[{i}]": sublist_findings})
+            elif isinstance(query, str) and query.lower() in item.lower() or \
+                    isinstance(query, int) and query == item:
+                findings.append({f"index[{i}]": item})
+        return findings
+
+    @staticmethod
+    def _search_dict(data_dict, query):
+        findings = []
+        for key, value in data_dict.items():
+            if isinstance(value, (list, dict)):
+                sublist_findings = search(value, query)
+                if sublist_findings:
+                    findings.append({key: sublist_findings})
+            elif (isinstance(query, str) and query.lower() in value.lower() or
+                  isinstance(key, str) and query.lower() in key.lower()) or \
+                    (isinstance(query, int) and query == value or isinstance(key, int) and query == key):
+                findings.append({key: value})
+        return findings
 
     def count(self, param):
         """
